01c34813a6622894d1951f6239e2beb1f278ad78
AndresRios123 <andresc.rios@campusucc.edu.co>
2026-06-03 22:04:57 -0500

    app.js actualizada

---
+++ src/app.js
@@ -0,0 +1,21 @@
+//Importar dependencias
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
+
+//Crear la app
+const app = express();
+
+//Middlewares globales
+app.use(cors());
+app.use(exxpress.json());
+app.use(morgan('dev'));
+
+//Ruta de health check
+app.get('/api/health', (req, res) => {
+    res.json({ status: 'ok', timestamp: new Date().toISOString() })
+});
+
+//Exportar
+module.exports = app;
