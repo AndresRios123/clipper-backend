e01e77ddbe8bb62f54011ee6015471eaac8afdfa
AndresRios123 <andresc.rios@campusucc.edu.co>
2026-06-19 15:29:03 -0500

    Agregada authorización para ver clientes - clientRoutes.js

--- src/routes/clientRoutes.js
+++ src/routes/clientRoutes.js
@@ -5,7 +5,7 @@ const {register, getClients} = require('../controllers/clientController');
 const { protect } = require('../middlewares/authMiddleware');
 const { authorize } = require('../middlewares/roleMiddleware');
 
-router.get('/clients', protect, authorize('admin'). getClients)
-router.post('/register/client', register)
+router.get('/clients', protect, authorize('admin', 'barbero'), getClients)
+router.post('/clients', protect, authorize('admin', 'barbero'), register)
 
 module.exports = router
