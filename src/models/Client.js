e80fe194371b6aad0699435bccd919ca861825fe
AndresRios123 <andresc.rios@campusucc.edu.co>
2026-06-19 12:58:12 -0500

    Se ha creado el modelo Client - CLient.js

--- src/models/Client.js
+++ src/models/Client.js
@@ -1,1 +1,30 @@
 //Schema y modelo del cliente
+const mongoose = require('mongoose');
+
+const clientSchema = new mongoose.Schema({
+    nombre: {
+        type: String,
+        required: true,
+    },
+    telefono: {
+        type: String,
+        required: true,
+        maxLength: 15,
+    },
+    email: {
+        type: String,
+        maxLength: 254,
+        unique: true,
+    },
+    direccion: {
+        type: String,
+    },
+    notas: {
+        type: String,
+    },
+},
+{timestamps: true});
+
+const Client = mongoose.model('Client', clientSchema);
+
+module.exports = Client
