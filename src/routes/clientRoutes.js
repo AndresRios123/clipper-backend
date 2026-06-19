69ad9901dd5ab637fc94c70a57e9d8a6bfe0dc41
AndresRios123 <andresc.rios@campusucc.edu.co>
2026-06-19 15:23:02 -0500

    Agregado controlador en /clients - clientRoutes.js

--- src/routes/clientRoutes.js
+++ src/routes/clientRoutes.js
@@ -5,7 +5,7 @@ const {register, getClients} = require('../controllers/clientController');
 const { protect } = require('../middlewares/authMiddleware');
 const { authorize } = require('../middlewares/roleMiddleware');
 
-router.get('/clients', protect, authorize('admin'))
+router.get('/clients', protect, authorize('admin'). getClients)
 router.post('/register/client', register)
 
 module.exports = router
