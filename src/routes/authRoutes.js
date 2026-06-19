a94329a875ed35ab65d3ad1ba2fd3e7f92fc8aa7
AndresRios123 <andresc.rios@campusucc.edu.co>
2026-06-18 21:59:21 -0500

    Agregada ruta '/users' - authRoutes

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,7 +1,10 @@
 const express = require('express')
 const router = express.Router()
-const { register, login } = require('../controllers/authController')
+const { register, login, getUsers } = require('../controllers/authController')
+const {protect} = require('../middlewares/authMiddleware');
+const {authorize} = require('../middlewares/roleMiddleware');
 
+router.get('/users', protect, authorize('admin'), getUsers);
 router.post('/register', register)
 router.post('/login', login)
 
