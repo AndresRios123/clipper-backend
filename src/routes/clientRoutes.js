e9df1b85922eb0cd869c1ce97d8f5436c7c81363
AndresRios123 <andresc.rios@campusucc.edu.co>
2026-06-19 16:48:28 -0500

    corregido error de parentesis - clientRoutes.js

--- src/routes/clientRoutes.js
+++ src/routes/clientRoutes.js
@@ -7,7 +7,7 @@ const { authorize } = require('../middlewares/roleMiddleware');
 
 router.get('/clients', protect, authorize('admin', 'barbero'), getClients)
 router.post('/clients', protect, authorize('admin', 'barbero'), createClient)
-router.get('/clients/:id', protect, authorize('admin', 'barbero', getClientById))
+router.get('/clients/:id', protect, authorize('admin', 'barbero'), getClientById)
 router.put('/clients/:id', protect, authorize('admin', 'barbero'), updateClient)
 router.delete('/clients/:id', protect, authorize('admin', 'barbero'), deleteClient)
 
