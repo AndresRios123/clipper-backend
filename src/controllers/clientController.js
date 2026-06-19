00a997a9038c14eb4b360426ef3ecc2cb4de9869
AndresRios123 <andresc.rios@campusucc.edu.co>
2026-06-19 13:26:12 -0500

    Agregado register en el controlador del cliente - clientController.js

--- src/controllers/clientController.js
+++ src/controllers/clientController.js
@@ -1,1 +1,32 @@
-const client = require('../models/Client');
+const Client = require('../models/Client');
+
+const register = async (req, res) => {
+    try {
+        const {nombre, telefono, email, direccion, notas} = req.body;
+
+        if (email) {
+            const clientExist = await Client.findOne({email});
+    
+            if (clientExist) {
+                return res.status(400).json({message: 'El email ingresado ya se encuentra registrado'});
+            }             
+        }
+
+        const cliente = await Client.create({nombre, telefono, email, direccion, notas});
+
+        if(cliente){
+            return res.status(201).json({
+                _id: cliente._id,
+                nombre: cliente.nombre,
+                telefono: cliente.telefono,
+                email: cliente.email,
+                direccion: cliente.direccion,
+                notas: cliente.notas,
+            })
+        }
+    } catch (error) {
+        return res.status(500).json({error: error.message});
+    }
+}
+
+module.exports = {register};
