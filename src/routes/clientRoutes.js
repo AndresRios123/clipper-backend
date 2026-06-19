9008bce74c073b6831424fae6b505505f39bfc12
AndresRios123 <andresc.rios@campusucc.edu.co>
2026-06-19 16:46:10 -0500

    agregadas rutasget put y delete - clientRoutes.js

--- src/routes/clientRoutes.js
+++ src/routes/clientRoutes.js
@@ -7,5 +7,8 @@ const { authorize } = require('../middlewares/roleMiddleware');
 
 router.get('/clients', protect, authorize('admin', 'barbero'), getClients)
 router.post('/clients', protect, authorize('admin', 'barbero'), createClient)
+router.get('/clients/:id', protect, authorize('admin', 'barbero', getClientById))
+router.put('/clients/:id', protect, authorize('admin', 'barbero'), updateClient)
+router.delete('/clients/:id', protect, authorize('admin', 'barbero'), deleteClient)
 
 module.exports = router
