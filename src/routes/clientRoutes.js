272678a8e27e3385fbb5968c348719de3fe6713c
AndresRios123 <andresc.rios@campusucc.edu.co>
2026-06-19 15:13:24 -0500

    Rutas getClients y register creadas - clientRoutes.js

--- src/routes/clientRoutes.js
+++ src/routes/clientRoutes.js
@@ -1,1 +1,11 @@
 //Rutas para clientes
+const express = require('express');
+const router = express.Router();
+const {register, getClients} = require('../controllers/clientController');
+const { protect } = require('../middlewares/authMiddleware');
+const { authorize } = require('../middlewares/roleMiddleware');
+
+router.get('/clients', protect, authorize('admin'))
+router.post('/register/client', register)
+
+module.exports = router
