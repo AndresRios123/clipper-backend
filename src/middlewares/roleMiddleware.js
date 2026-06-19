42401227c612f326cc4300d2c6a1952a3e4fbea7
AndresRios123 <andresc.rios@campusucc.edu.co>
2026-06-18 21:45:28 -0500

    roleMiddleware creado

---
+++ src/middlewares/roleMiddleware.js
@@ -0,0 +1,10 @@
+// Recibe los roles permitidos y devuelve un middleware
+const authorize = (...roles) => {
+    return (req, res, next) => {
+        if(roles.includes(req.user.rol)){
+            next();
+        }else{
+            res.status(403).json({message: 'Acceso denegado. No tienes permisos'});
+        }
+    }
+}
