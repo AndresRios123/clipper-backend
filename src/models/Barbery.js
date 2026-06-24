f8592de9e31b9cf033ff7604dcab4792b802251d
AndresRios123 <andresc.rios@campusucc.edu.co>
2026-06-23 20:31:08 -0500

    Se ha creado el modelo barbería - Barbery.js

--- src/models/Barbery.js
+++ src/models/Barbery.js
@@ -1,3 +1,37 @@
 // Modelo para barberías
 const mongoose = require('mongoose')
 const bcrypt = require('bcrypt')
+
+const barberySchema = new mongoose.Schema({
+    nombre: {
+        type: String,
+        required: true
+    },
+    direccion: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    telefono: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+    },
+    activo: {
+        type: Boolean,
+        default: true,
+    },
+    duenio: {
+        type: mongoose.Schema.ObjectId,
+        ref: 'Usuario',
+        required: true,
+    }
+},
+{timestamps: true});
+
+const Barbery = mongoose.model('Barbery', barberySchema);
+
+module.exports = Barbery
